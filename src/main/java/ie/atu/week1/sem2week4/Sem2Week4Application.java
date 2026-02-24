3729f67ea60752e0819cc7594ce007d9116ffe98
Nandita <g00463584@atu.ie>
2026-02-24 19:32:29 +0000

    initial commit

---
+++ src/main/java/ie/atu/week1/sem2week4/Sem2Week4Application.java
@@ -0,0 +1,13 @@
+package ie.atu.week1.sem2week4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem2Week4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sem2Week4Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week1/sem2week4/Sem2Week4ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week1.sem2week4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem2Week4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
